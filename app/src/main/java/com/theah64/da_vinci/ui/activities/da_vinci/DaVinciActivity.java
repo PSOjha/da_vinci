6354a8da26b950e45dc90d1d4fe7a8cefc539e32
theapache64 <theapache64@gmail.com>
2018-10-07 14:30:34 +0530

    Added bitmap loader

--- app/src/main/java/com/theah64/da_vinci/ui/activities/da_vinci/DaVinciActivity.java
+++ app/src/main/java/com/theah64/da_vinci/ui/activities/da_vinci/DaVinciActivity.java
@@ -2,16 +2,23 @@ package com.theah64.da_vinci.ui.activities.da_vinci;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.theah64.da_vinci.R;
 import com.theah64.da_vinci.api.responses.GetShapesResponse;
-import com.theah64.da_vinci.ui.activities.base.BaseAppCompatActivity;
+import com.theah64.da_vinci.ui.activities.base.BaseProgressManActivity;
 import com.theah64.da_vinci.widgets.DaVinciCanvas;
 
 import butterknife.BindView;
 
-public class DaVinciActivity extends BaseAppCompatActivity {
+public class DaVinciActivity extends BaseProgressManActivity {
 
     @BindView(R.id.dvc)
     DaVinciCanvas dvc;
@@ -25,6 +32,28 @@ public class DaVinciActivity extends BaseAppCompatActivity {
 
         final GetShapesResponse.Shape defaultShape = new GetShapesResponse.Shape("1", "http://theapache64.com:8090/mock_api_data/1538879648096_PmHwp3duAX.png");
         final GetShapesResponse.Shape shape = (GetShapesResponse.Shape) getSerializableExtra(KEY_SHAPE, defaultShape);
+
+        showLoading("Loading shape...");
+
+        Glide.with(this)
+                .asBitmap()
+                .load(shape.getImageUrl())
+                .addListener(new RequestListener<Bitmap>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Bitmap> target, boolean isFirstResource) {
+                        hideLoading();
+                        showToast("Failed");
+                        finish();
+                        return true;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Bitmap resource, Object model, Target<Bitmap> target, DataSource dataSource, boolean isFirstResource) {
+                        hideLoading();
+                        showToast("Ready!");
+                        return true;
+                    }
+                }).submit();
 
     }
 
